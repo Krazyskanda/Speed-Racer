d78cafe3179cf9587d6a8ee9b8fc3e2e6ae45c64
Krazyskanda <86784699+Krazyskanda@users.noreply.github.com>
2021-10-21 18:55:37 +0530

    Add files via upload

--- js/Game.js
+++ js/Game.js
@@ -7,6 +7,7 @@ class Game {
 
     this.leader1 = createElement("h2");
     this.leader2 = createElement("h2");
+    this.playerMoving = false;
   }
 
   getState() {
@@ -95,6 +96,8 @@ class Game {
     if (allPlayers !== undefined) {
       image(track, 0, -height * 5, width, height * 6);
       this.showLeaderboard();
+      this.showLife();
+      this.showFuelBar();
 
       //index of the array
       var index = 0;
@@ -124,24 +127,39 @@ class Game {
 
         }
       }
+      if (this.playerMoving) {
+        player.positionY += 5;
+        player.update();
+      }
 
       // handling keyboard events
       if (keyIsDown(UP_ARROW)) {
         player.positionY += 10;
         player.update();
       }
-      this.handlePlayerControls
+      this.handlePlayerControls();
       const finishLine = height * 6 - 100;
 
       if (player.positionY > finishLine) {
-gameState = 2;
-player.rank += 1;
-Player.updateCarsAtEnd(player.rank);
-player.update();
-this.showRank();
+        gameState = 2;
+        player.rank += 1;
+        Player.updateCarsAtEnd(player.rank);
+        player.update();
+        this.showRank();
       }
+
       drawSprites();
     }
+  }
+  showFuelBar() {
+    push();
+    image(fuelImage, width / 2 - 130, height - player.positionY - 100, 20, 20);
+    fill("white");
+    rect(width / 2 - 100, height - player.positionY - 100, 185, 20);
+    fill("#ffc400");
+    rect(width / 2 - 100, height - player.positionY - 100, player.fuel, 20);
+    noStroke();
+    pop();
   }
 
   handleFuel(index) {
@@ -152,6 +170,14 @@ this.showRank();
       //the event
       collected.remove();
     });
+    if (player.fuel > 0 && this.playerMoving) {
+      player.fuel -= 0.3;
+    }
+
+    if (player.fuel <= 0) {
+      gameState = 2;
+      this.gameOver();
+    }
   }
 
   handlePowerCoins(index) {
@@ -173,6 +199,17 @@ handleResetButton() {
     });
     window.location.reload();
   });
+}
+
+showLife() {
+  push();
+  image(lifeImage, width / 2 - 130, height - player.positionY - 400, 20, 20);
+  fill("white");
+  rect(width / 2 - 100, height - player.positionY - 400, 185, 20);
+  fill("#f50057");
+  rect(width / 2 - 100, height - player.positionY - 400, player.life, 20);
+  noStroke();
+  pop();
 }
 showLeaderboard() {
   var leader1, leader2;
@@ -219,6 +256,8 @@ showLeaderboard() {
 
 handlePlayerControls() {
   if (keyIsDown(UP_ARROW)) {
+    this.playerMoving = true; //C40 //SA
+
     player.positionY += 10;
     player.update();
   }
@@ -234,15 +273,22 @@ handlePlayerControls() {
   }
 }
 showRank() {
-  swal({                                                       
-    title:`Awesome!${"\n"}Rank${"\n"}${player.rank}`,
+  swal({
+    title: `Awesome!${"\n"}Rank${"\n"}${player.rank}`,
     text: "You reached the finish line successfully",
     imageUrl:
-    " https://raw.githubusercontent.com/vishalgaddam873/p5-multiplayer-car-race-game/master/assets/cup.png",
+      "https://raw.githubusercontent.com/vishalgaddam873/p5-multiplayer-car-race-game/master/assets/cup.png",
     imageSize: "100x100",
     confirmButtonText: "Ok"
-  })                                                                                                                  
-}       
-}                                                        
-
-                                                                  
+  });}
+  gameOver() {
+    swal({
+      title: `Game Over`,
+      text: "Oops you lost the race....!!!",
+      imageUrl:
+        "https://cdn.shopify.com/s/files/1/1061/1924/products/Thumbs_Down_Sign_Emoji_Icon_ios10_grande.png",
+      imageSize: "100x100",
+      confirmButtonText: "Thanks For Playing"
+    });
+  }
+}
